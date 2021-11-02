2464ce88cb0c81209554602cf08dbe42d81ba5c6
majedqatousa <qatousa.1142005@gmail.com>
2021-11-02 13:09:02 +0200

    change main image roul

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -32,7 +32,7 @@ class CategoryRequest extends FormRequest
 
             case 'POST':
                 return [
-                    'image' => 'required|image',
+                    // 'image' => 'required|image',
                     'name' => 'required',
                     'order' => 'required|numeric|unique:categories,order|min:1',
 
