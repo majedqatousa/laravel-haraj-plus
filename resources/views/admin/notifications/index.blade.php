32976af2325d76bbcb505a973392352ff6077162
majedqatousa <qatousa.1142005@gmail.com>
2021-11-04 21:48:46 +0200

    notifications 16

--- resources/views/admin/notifications/index.blade.php
+++ resources/views/admin/notifications/index.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('admin.layouts.app')
 
 @section('pageTitle')لوحة التحكم
@@ -9,58 +7,53 @@
 @endsection
 
 @section('content')
+<!--start row-->
+<div class="row">
+    <!--start div-->
+    <div class="breadcrumbes col-12">
+        <ul class="list-inline">
+            <li><a href="{{route('main')}}"><i class="fa fa-home"></i>الرئيسية</a></li>
+            <li>الإشعارات</li>
+        </ul>
+    </div>
     <!--start row-->
     <div class="row">
-        <!--start div-->
-        <div class="breadcrumbes col-12">
-            <ul class="list-inline">
-                <li><a href="{{route('main')}}"><i class="fa fa-home"></i>الرئيسية</a></li>
-                <li>الإشعارات</li>
-            </ul>
-        </div>
-        <!--start row-->
-    <div class="row">
-        <!--start div-->
-      
-        <!--end div-->
-
 
         <!--start div-->
         <div class="col-md-12 clients-grid margin-bottom-div">
             <div class="main-white-box">
                 <h3 class="sec-title color-title"><span>إرسال إشعار</span></h3>
                 @include('alert')
-                <form class="needs-validation row border-form" id="myform" novalidate="" method="post" enctype="multipart/form-data" action="{{route('sendNotification')}}"
-                >
+                <form class="needs-validation row border-form" id="myform" novalidate="" method="post" enctype="multipart/form-data" action="{{route('sendNotification')}}">
                     @csrf
 
 
 
                     <div class="form-group  col-md-6">
                         <label>العنوان<span class="starrisk">*</span></label>
-                        <input type="text" class="form-control"  name="title" required>
+                        <input type="text" class="form-control" placeholder="العنوان" name="title" required>
                         <div class="invalid-feedback">
                             من فضلك أدخل العنوان
                         </div>
                         @if ($errors->has('image'))
-                            <div style="display:block;" class="invalid-feedback">{{$errors->first('image') }}</div>
+                        <div style="display:block;" class="invalid-feedback">{{$errors->first('image') }}</div>
                         @endif
-                                         
+
                     </div>
 
                     <div class="form-group  col-md-6">
                         <label>الإشعار<span class="starrisk"></span></label>
-                        <input type="text" class="form-control" placeholder="العنوان"  name="body" >
+                        <input type="text" class="form-control" placeholder="الإشعار" name="body">
                         <div class="invalid-feedback">
                             من فضلك أدخل الإشعار
                         </div>
                         @if ($errors->has('title'))
-                            <div style="display:block;" class="invalid-feedback">{{$errors->first('title') }}</div>
+                        <div style="display:block;" class="invalid-feedback">{{$errors->first('title') }}</div>
                         @endif
-                      
+
                     </div>
 
-                
+
 
 
                     <div class="form-group  margin-top-div text-center col-12">
@@ -77,8 +70,8 @@
 
     </div>
     <!--end row-->
-    </div>
-    <!--end row-->
+</div>
+<!--end row-->
 
 
 
@@ -93,6 +86,3 @@
 
 <!-- scripts
      ================ -->
-
-
-
