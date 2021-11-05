8779924360c5f27d9c915e33a2a3e305105c0cbd
majedqatousa <qatousa.1142005@gmail.com>
2021-11-05 17:28:27 +0200

    notifications 28

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\API\UserFcmTokenController;
-use Session;
+
 
 class NotificationController extends Controller
 {
@@ -16,9 +16,7 @@ class NotificationController extends Controller
     public function send(Request $request){
         $fcmNotification = new UserFcmTokenController();
         $fcmNotification->sendToAll($request->get('title') , $request->get('body'));
-        $request->session()->flash('notification-status', ' تم إرسال الإشعار بنجاح');
+        $request->session()->put('notification-status', ' تم إرسال الإشعار بنجاح');
         return redirect('admin/notifications');
-
-
     }
 }
