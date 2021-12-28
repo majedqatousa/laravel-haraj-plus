f9f13e61603ed5254bb69c695088b4c19327b44c
majedqatousa <qatousa.1142005@gmail.com>
2021-12-28 08:17:35 +0200

    fillter data 19

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -273,6 +273,11 @@ class HomeController extends Controller
              ->WhereBetween('created_at', [$fromDate, $toDate])
              ->whereBetween('price', [$fromPrice, $toPrice])
             ->get();
+        }else{
+            $productCreated = Product::where('category_id', $category_id)->where('city_id', $city_id)
+            ->WhereBetween('created_at', [$fromDate, $toDate])
+            ->whereBetween('price', [$fromPrice, $toPrice])
+           ->get();
         }
         // else if(is_null($fromPrice) && is_null($toPrice)){
         //     $productCreated = Product::where('category_id', $category_id)
