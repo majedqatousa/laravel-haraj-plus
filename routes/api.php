4c9835f24109957524162dae7ca1df8358a96bee
majedqatousa <qatousa.1142005@gmail.com>
2022-02-27 01:20:32 +0200

    fillter 9

--- routes/api.php
+++ routes/api.php
@@ -80,7 +80,7 @@ Route::get('userProduct', 'API\HomeController@userProduct');
 Route::get('productCreated', 'API\HomeController@productCreated');
 Route::get('productPrice/{type?}', 'API\HomeController@productPriceDesc');
 Route::get('productPriceASC', 'API\HomeController@productPriceASC');
-Route::get('fillterData', 'API\HomeController@fillterData');
+Route::post('fillterData', 'API\HomeController@fillterData');
 Route::get('userPromit', 'API\HomeController@userPromit');
 Route::get('productUserPromit', 'API\HomeController@productUserPromit');
 Route::get('user-product', 'API\HomeController@productUser');
