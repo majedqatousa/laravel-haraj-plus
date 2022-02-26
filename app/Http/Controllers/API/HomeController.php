ace10bafaa50b21756e6efd7eb7b02ed42d0c609
majedqatousa <qatousa.1142005@gmail.com>
2022-02-27 01:32:29 +0200

    fillter 11

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -272,7 +272,7 @@ class HomeController extends Controller
                     // ->WhereBetween('created_at', [$fromDate, $toDate])
                     ->paginate(10);
 
-                    $extra = "2 is statment "; 
+                    $extra = "3 is statment "; 
         }
         else if(is_null($fromPrice) && is_null($toPrice) && is_null($category_id) && !is_null($city_id) && is_null($fromDate) && is_null($toDate)){
             $productCreated = Product::where("is_valid" , 1)
@@ -282,7 +282,7 @@ class HomeController extends Controller
                     // ->WhereBetween('created_at', [$fromDate, $toDate])
                     ->paginate(10);
 
-                    $extra = "2 is statment "; 
+                    $extra = "4 is statment "; 
         }
         else{
             $productCreated = Product::where("is_valid" , 1)
@@ -298,7 +298,7 @@ class HomeController extends Controller
                 'status' => true,
                 'message' => 'Success',
                 'extra' => $extra,
-                'data 2 '=> $fromDate." - ". $toDate." - ". $fromPrice . "-". $toPrice,
+                'data 2 '=> $fromDate." - ". $toDate." - ". $fromPrice . "-". $toPrice."  -  ". $category_id . "  -  ".$city_id ,
                 'data' => $productCreated
             ]);
 
