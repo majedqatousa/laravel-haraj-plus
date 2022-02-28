4ec781870e947d3fd1cd19d43cb4f4f15e93ec79
majedqatousa <qatousa.1142005@gmail.com>
2022-02-28 17:03:26 +0200

    fillter 35

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -247,7 +247,7 @@ class HomeController extends Controller
         $city_id = $request->get('city_id');
        
         $extra = "else statment ";
-        if(!isset($fromPrice)  && !isset($toPrice) && !isset($category_id)  && !isset($city_id)&& isset($fromDate) && isset($toDate) ){
+        if(is_null($fromPrice)  && is_null($toPrice) && is_null($category_id)  && is_null($city_id)&& !is_null($fromDate) && !is_null($toDate) ){
             $productCreated = Product::where("is_valid" , 1)
                     // ->where('category_id', $category_id)
                     // ->where('city_id', $city_id)
